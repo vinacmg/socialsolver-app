dd35158a5429498e7ef78dc9e742538b2ccea305
unknown <phms.1998@gmail.com>
2018-06-01 22:33:22 -0300

    Ordenação dos comentários (do mais recente ao mais antigo)

--- src/pages/report-detail-modal/report-detail-modal.ts
+++ src/pages/report-detail-modal/report-detail-modal.ts
@@ -44,6 +44,11 @@ export class ReportDetailModalPage {
   ionViewDidLoad() {
     this.fire.getComentarios(this.report).subscribe(commentsItem => {
       this.comments = commentsItem;
+      this.comments.sort(function(a, b) {
+        if (a.data > b.data) return -1;
+        if (a.data < b.data) return 1;
+        return 0;
+      });
     });
   }
 
